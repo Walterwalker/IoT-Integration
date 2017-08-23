7ae0db5fea6cdd6e4d159c1467e54ec05e0ed429
walterwalker <stsurapa@uwaterloo.ca>
2017-08-23 02:39:40 -0400

    documenting
    part 1

---
+++ main.py
@@ -0,0 +1,3 @@
+# First step is data collection
+# Lets start with collecting data from the smart band
+# We have two options 1. Using MiFit App ( I am using Mi Band 2 reason being dead cheap and better customization ). 2.Google Fit Data
